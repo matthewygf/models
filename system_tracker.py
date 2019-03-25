f7dd85be3b2a668a3410b2a14259bb1954d9f4f7
matthew <yyygggfff@hotmail.com>
2019-03-25 13:52:19 +0000

    add info to log

--- system_tracker.py
+++ system_tracker.py
@@ -26,17 +26,17 @@ import time
 import traceback
 import psutil
 
-
 class SystemInfoTracker(object):
   """Keep track of system information such as maximum memory usage with separate thread."""
 
   def __init__(self, output_dir):
-    self.process_info_log = open(os.path.join(output_dir, 'system.log'), 'w+')  # pylint: disable=line-too-long
+    self.system_info_log = open(os.path.join(output_dir, 'system.log'), 'w+')  # pylint: disable=line-too-long
     self.scheduler = sched.scheduler(time.time, time.sleep)
-    self.process_info = {}
-    self.process_info['average_mem_percent'] = 0
-    self.process_info['max_cpu_percent'] = 0
-    self.process_info['average_cpu_percent'] = 0
+    self.system_info = {}
+    self.system_info['average_mem_percent'] = 0
+    self.system_info['max_cpu_percent'] = 0
+    self.system_info['average_cpu_percent'] = 0
+
     self.log_times = 0
     self.exit_event = threading.Event()
     self.last_exception = None
@@ -44,21 +44,21 @@ class SystemInfoTracker(object):
 
   def start(self):
     self.start_time = time.time()
-    self.scheduler.enter(1, 1, self._update_process_info)  # pylint: disable=no-value-for-parameter
+    self.scheduler.enter(1, 1, self._update_system_info)  # pylint: disable=no-value-for-parameter
     threading.Thread(target=self.scheduler.run).start()
     logging.info('Started process information tracker.')
 
   def stop(self):
     self.exit_event.set()
-    self.process_info_log.flush()
+    self.system_info_log.flush()
     logging.info('Stopped process information tracker.')
 
     if self.last_exception is not None:
       raise self.last_exception  # pylint: disable=raising-bad-type
 
-    return dict(self.process_info)
+    return dict(self.system_info)
 
-  def _update_process_info(self, ):
+  def _update_system_info(self, ):
     """Read and update process info using background thread every 1 second."""
 
     try:
@@ -66,23 +66,23 @@ class SystemInfoTracker(object):
       # This affects the interval 
       # at which the metrics are reported
       cpu_percent = psutil.cpu_percent(interval=0.1)
-      mem_percent = psutil.memory_percent()
-      mean_mem = (self.log_times * self.process_info['average_mem_percent']) + mem_percent
-      mean_cpu = (self.log_times * self.process_info['average_cpu_percent']) + cpu_percent
-      self.process_info['average_mem_percent'] = mean_mem / (self.log_times + 1)
-      self.process_info['average_cpu_percent'] = mean_cpu / (self.log_times + 1)
-      self.process_info['max_cpu_percent'] = max(self.process_info['max_cpu_percent'], cpu_percent)  # pylint: disable=line-too-long
+      mem_percent = psutil.virtual_memory().percent
+      mean_mem = (self.log_times * self.system_info['average_mem_percent']) + mem_percent
+      mean_cpu = (self.log_times * self.system_info['average_cpu_percent']) + cpu_percent
+      self.system_info['average_mem_percent'] = mean_mem / (self.log_times + 1)
+      self.system_info['average_cpu_percent'] = mean_cpu / (self.log_times + 1)
+      self.system_info['max_cpu_percent'] = max(self.system_info['max_cpu_percent'], cpu_percent)  # pylint: disable=line-too-long
       self.log_times += 1
       entry = {}
       entry['time'] = time.time() - self.start_time
       entry['cpu_percent'] = cpu_percent
       entry['mem_percent'] = mem_percent
-      entry['average_cpu_percent'] = self.process_info['average_cpu_percent']
-      entry['average_mem_percent'] = self.process_info['average_mem_percent']
-      self.process_info_log.write(json.dumps(entry) + '\n')
+      entry['average_cpu_percent'] = self.system_info['average_cpu_percent']
+      entry['average_mem_percent'] = self.system_info['average_mem_percent']
+      self.system_info_log.write(json.dumps(entry) + '\n')
       if not self.exit_event.is_set():
         # Schedule the next event to be run after 1 second
-        self.scheduler.enter(1, 1, self._update_process_info)  # pylint: disable=no-value-for-parameter
+        self.scheduler.enter(1, 1, self._update_system_info)  # pylint: disable=no-value-for-parameter
     except Exception as e:  # pylint: disable=W0703
       logging.error('System info tracker failed due to error:\n %s',
                     traceback.format_exc())
