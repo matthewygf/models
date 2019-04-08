47f22fff502c84f35fd06120044742e1e7661260
matthew <yyygggfff@hotmail.com>
2019-04-08 20:38:31 +0100

    test

--- research/slim/train_image_classifier.py
+++ research/slim/train_image_classifier.py
@@ -234,7 +234,7 @@ tf.app.flags.DEFINE_boolean(
 #####################
 
 tf.app.flags.DEFINE_float(
-    'gpu_memory_fraction', 0.99,
+    'gpu_memory_fraction', 0.98,
     'value from 0 to 1, if less than 0 than will error of course :P'
 )
 
