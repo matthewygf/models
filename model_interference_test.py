868ff22649893b62b88d47813f6d49500dd38cc6
matthew <yyygggfff@hotmail.com>
2019-04-09 17:42:00 +0100

    test

--- model_interference_test.py
+++ model_interference_test.py
@@ -292,17 +292,26 @@ def run(
 def main():
     # which one we should run in parallel
     sets = [
-            ['mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32'], 
-            # ['resnet_v1_50_batch_32'], 
-            # ['resnet_101_v1_batch_32'],
-            # ['resnet_151_v1_batch_32'],
-            # ['vgg19_batch_32'], 
-            # ['vgg16_batch_32'],
-            # ['alexnet_v2_batch_32'],
-            # ['inceptionv1_batch_32'],
+            ['mobilenet_v1_025_batch_32'],
+            ['mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32'],
+            ['mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32'],
+            ['resnet_v1_50_batch_32'], 
+            ['resnet_v1_50_batch_32', 'resnet_v1_50_batch_32']
+            ['resnet_v1_50_batch_32', 'ptb_word_lm'],
+            ['ptb_word_lm'],
+            ['ptb_word_lm', 'ptb_word_lm']
+            ['ptb_word_lm', 'mobilenet_v1_025_batch_32']
+            ['resnet_101_v1_batch_32'],
+            ['resnet_151_v1_batch_32'],
+            ['vgg19_batch_32'], 
+            ['vgg16_batch_32'],
+            ['inceptionv1_batch_32'],
+            ['inceptionv1_batch_32', 'inceptionv1_batch_32']
+            [ 'ptb_word_lm', 'inceptionv1_batch_32']
             # ['inceptionv2_batch_32'],
             # ['inceptionv3_batch_32'],
             # ['inceptionv4_batch_32']
+            # ['alexnet_v2_batch_32'],
            ]
     project_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     experiment_path = os.path.join(project_dir, 'experiment')
