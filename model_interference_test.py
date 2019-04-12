f18fbc55346bc1330e3bd1fd71332df31352af19
matthew <yyygggfff@hotmail.com>
2019-04-12 14:54:38 +0100

    test

--- model_interference_test.py
+++ model_interference_test.py
@@ -243,7 +243,7 @@ def run(
                     current_time = time.time()
                     executed = current_time - start_time
                     print("checking the time, process %d been running for %d " % (pid,executed))
-                    if executed >= 60.0 * 4:
+                    if executed >= 60.0 * 30:
                         # make sure we profile a few mins.
                         # to observe the interference
                         p.kill()
