0fd27f8389dea3b4778ec8a2c02ef5700387c045
matthew <yyygggfff@hotmail.com>
2019-04-12 17:00:59 +0100

    test

--- research/slim/train_image_classifier.py
+++ research/slim/train_image_classifier.py
@@ -18,10 +18,6 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
 
 from datasets import dataset_factory
--- tutorials/rnn/ptb/ptb_word_lm.py
+++ tutorials/rnn/ptb/ptb_word_lm.py
@@ -60,10 +60,6 @@ $ python ptb_word_lm.py --data_path=simple-examples/data/
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import time
 
 import numpy as np
