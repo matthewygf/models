59ab30b339a7ae22b40272a5ba2ce3837c63bd36
matthew <yyygggfff@hotmail.com>
2019-04-25 14:25:14 +0100

    typo

--- model_interference_test.py
+++ model_interference_test.py
@@ -311,7 +311,7 @@ def run(
 def main():
     # which one we should run in parallel
     sets = [
-            ['resnet_50_v1_b8_cmd']
+            ['resnet_v1_50_batch_8']
             # ['mobilenet_v1_025_batch_32'],
             # ['mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32'],
             # ['mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32', 'mobilenet_v1_025_batch_32'],
