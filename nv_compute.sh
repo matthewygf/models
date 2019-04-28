3ee87c824309cd5cf91149c2fec49124c8bb5122
matthew <yyygggfff@hotmail.com>
2019-04-28 14:33:02 +0100

    wrong args

--- nv_compute.sh
+++ nv_compute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 timestamp=$(date +%s)
-/usr/local/NVIDIA-Nsight-Compute/nv-nsight-cu-cli --o=$timestamp --f --profile-from-start=off --target-processes=all ./test.sh
+/usr/local/NVIDIA-Nsight-Compute/nv-nsight-cu-cli -o=$timestamp -f --profile-from-start=off --target-processes=all ./test.sh
